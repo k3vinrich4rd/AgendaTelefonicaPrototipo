948d3c02d9a6b3188ceadf17172eea4f04ade396
k3vinrich4rd <kevinrichardve@gmail.com>
2022-08-12 16:09:27 -0300

    Inserindo alterações

--- src/main/java/com/AgendaDeContatos/AgendaContatos/controller/ContatosController.java
+++ src/main/java/com/AgendaDeContatos/AgendaContatos/controller/ContatosController.java
@@ -1,4 +1,44 @@
 package com.AgendaDeContatos.AgendaContatos.controller;
 
+import com.AgendaDeContatos.AgendaContatos.model.ContatosModel;
+import com.AgendaDeContatos.AgendaContatos.service.ContatosService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class ContatosController {
+
+    @Autowired
+    ContatosService contatosService;
+
+    @GetMapping(path = "/contatos")
+    public List<ContatosModel> mostrarContatos() {
+        return contatosService.exibirTodosContatos();
+    }
+
+    @GetMapping(path = "/contatos/{codigo}")
+    public Optional<ContatosModel> exibirViaId(@PathVariable Long codigo) {
+        return contatosService.exibirViaId(codigo);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ContatosModel cadastrarContatos(@RequestBody ContatosModel contatosModel) {
+        return contatosService.cadastrarContatos(contatosModel);
+    }
+
+    @PutMapping
+    public ContatosModel alterarContatos(@RequestBody ContatosModel contatosModel) {
+        return contatosService.alterarContatos(contatosModel);
+    }
+
+    @DeleteMapping
+    public void deletarContatos(@PathVariable Long codigo) {
+        contatosService.deletarContatos(codigo);
+    }
+
 }
--- src/main/java/com/AgendaDeContatos/AgendaContatos/service/ContatosService.java
+++ src/main/java/com/AgendaDeContatos/AgendaContatos/service/ContatosService.java
@@ -1,4 +1,41 @@
 package com.AgendaDeContatos.AgendaContatos.service;
 
+import com.AgendaDeContatos.AgendaContatos.model.ContatosModel;
+import com.AgendaDeContatos.AgendaContatos.repository.ContatosRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ContatosService {
+
+    @Autowired
+    private ContatosRepository contatosRepository;
+
+
+    public List<ContatosModel> exibirTodosContatos() {
+        return contatosRepository.findAll();
+    }
+
+
+    public Optional<ContatosModel> exibirViaId(Long codigo) {
+        return contatosRepository.findById(codigo);
+
+    }
+
+    public ContatosModel cadastrarContatos(ContatosModel contatosModel) {
+        return contatosRepository.save(contatosModel);
+    }
+
+
+    public ContatosModel alterarContatos(ContatosModel contatosModel) {
+        return contatosRepository.save(contatosModel);
+    }
+
+
+    public void deletarContatos(Long codigo) {
+        contatosRepository.deleteById(codigo);
+    }
 }
